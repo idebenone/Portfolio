e22fce495f46b3faf2f224432b849aa65b4eeabd
Vineeth <vineeth.v.gooli@gmail.com>
2023-06-19 19:38:49 +0530

    protean

--- main.js
+++ main.js
@@ -3,4 +3,5 @@ $(document).ready(function () {
         $(".waterfall").css({ "opacity": '0', "transition": "0.5s ease-in-out" });
         $("#container").css({ "display": "block", "opacity": '1', "transition": "0.5s ease-in-out" });
     }, 5000)
+
 })
