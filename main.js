c779056e421eca61784457cc2e09b53066982fbc
Vineeth <vineeth.v.gooli@gmail.com>
2023-11-20 01:12:50 +0530

    projects update

--- main.js
+++ main.js
@@ -2,6 +2,6 @@ $(document).ready(function () {
     setTimeout(() => {
         $(".waterfall").css({ "opacity": '0', "transition": "0.5s ease-in-out" });
         $("#container").css({ "display": "block", "opacity": '1', "transition": "0.5s ease-in-out" });
-    }, 5000)
+    }, 2500)
 
 })
